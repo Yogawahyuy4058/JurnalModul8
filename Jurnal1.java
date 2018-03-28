06aca26be7b55d6fe3130ff337917c965e946e74
Yoga Wahyu Yuwono <37763772+Yogawahyuy4058@users.noreply.github.com>
2018-03-28 11:06:46 +0700

    Kodingan Java Jurnal No 1

---
+++ Jurnal1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jurnal.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class Jurnal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+  
+    public static void main(String[] args) {
+        Scanner yo=new Scanner(System.in);
+        System.out.print("Masukan angka :");
+        int input=yo.nextInt()+1;
+        long fibo[]=new long[input];
+        fibo[0]=0;
+        fibo[1]=1;
+        System.out.println("Output :");
+        for (int i = 2; i < input; i++) {
+           fibo[i]=fibo[i-1]+fibo[i-2];
+            
+        }
+        for (int i = 0; i < input; i++) {
+            System.out.print(fibo[i]+ " ");
+            
+        }
+        // TODO code application logic here
+    }
+    
+}
